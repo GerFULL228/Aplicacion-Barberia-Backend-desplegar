455749b6e96a7060077f3b1b8ac99aa7ac2400e7
Dejoda <U23216205@utp.edu.pe>
2026-05-24 16:15:59 -0500

    feat: Agregar endpoints para listar roles permisos usuarios-table
    -Dependencias para generacion de QR
    -listar usuarios-table
    -asignar roles
    -listar roles
    - listar permisos

--- src/main/java/com/sistemabarberia/fadex_backend/modules/barbero/dto/response/BarberoResponseDTO.java
+++ src/main/java/com/sistemabarberia/fadex_backend/modules/barbero/dto/response/BarberoResponseDTO.java
@@ -11,10 +11,7 @@ import java.time.LocalDate;
 @Builder
 public class BarberoResponseDTO {
     private Integer barberoId;
-    private String nombre;
-    private String apellido;
-    private String telefono;
-    private String email;
+    private Persona persona;
     private Integer experiencia;
     private LocalDate fechaIngreso;
     private boolean ocupado;
